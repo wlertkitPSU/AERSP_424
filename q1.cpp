761da00a12b39657262c02f20b747b3bb5816dc9
wlertkitPSU <wkl5183@psu.edu>
2024-09-30 19:32:17 -0400

    hardcoded cg

---
+++ q1.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <array>
+
+using namespace std;
+
+// hard coded values
+float plane_weight = 2050;
+float plane_moment = 155400;
+int front_num = 2;
+float front_weight = 330;
+float front_moment_arm = 85;
+int rear_num = 2;
+float rear_weight = 330;
+float rear_moment_arm = 121;
+float max_fuel = 44;
+float fuel_weight_density = 6;
+float fuel_moment_arm = 75;
+float baggage_weight = 10;
+float baggage_moment_arm = 140;
+float fuel_weight = max_fuel * fuel_weight_density;
+
+// values that need to be found
+float total_weight;
+float total_moment;
+float cg;
+
+int main(){
+    total_weight = plane_weight + front_weight + rear_weight + baggage_weight + fuel_weight;
+    total_moment = plane_moment + (front_weight * front_moment_arm) + (rear_weight * rear_moment_arm) + (baggage_weight + baggage_moment_arm) + (fuel_weight + fuel_moment_arm);
+    cg = total_moment/total_weight;
+    cout << total_weight << endl;
+    cout << total_moment << endl;
+    cout << cg << endl;
+    return 0;
+}
