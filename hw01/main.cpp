d2a5d5a0b02cc94fc6deb89a1d22c882977b2dd4
Emily Hartman <hartmane19@gmail.com>
2024-10-16 18:11:58 -0400

    Question 5 done

--- hw01/main.cpp
+++ hw01/main.cpp
@@ -5,8 +5,14 @@
 using namespace std;
 
 int main(){
-    map<string, map<string, int>> test;
-    test = distance_init();
-    cout << 1 << endl;
+    Plane myplane;
+    myplane.setVelocity(400);
+    int timestep = 10;
+    int maxiterations = 1000;
+    for (int i = 0; i < maxiterations; ++i){
+        myplane.operate(timestep);
+        cout << timestep * i << " " << myplane.getPosition() << endl;
+
+    }
     return 0;
 }
--- hw01/q34.cpp
+++ hw01/q34.cpp
@@ -5,6 +5,11 @@
 Plane::Plane() {
     cin >> origin >> destination;
     distance_map = distance_init();
+    pos = 0;
+    vel = 0;
+    at_SCE = 0;
+    distance = distance_map[origin][destination];
+    cout << "Plane Created at [adress]";
 }
 
 // destructor
@@ -15,12 +20,12 @@ Plane::~Plane() {
 // operate function
 void Plane::operate(double dt) {
     if (pos < distance_map[origin][destination]) {
-        pos += vel * dt;
-        at_SCE = false;
+        pos += vel/3600 * dt;
+        at_SCE = 0;
     } else {
         if (destination == "SCE") {
             string temp;
-            at_SCE = true;
+            at_SCE = 1;
             temp = origin;
             origin = destination;
             destination = temp;
