ebab5f5d522bf9c19aaa40e4b4de555b1585b7cf
Emily Hartman <hartmane19@gmail.com>
2024-10-16 21:43:35 -0400

    Question 5 updated

--- hw01/main.cpp
+++ hw01/main.cpp
@@ -6,12 +6,26 @@ using namespace std;
 
 int main(){
     Plane myplane;
-    myplane.setVelocity(400);
-    int timestep = 10;
-    int maxiterations = 1000;
-    for (int i = 0; i < maxiterations; ++i){
+
+    // Setting the plane's velocity (in miles per hour)
+    double velocity;
+    cout << "Enter the plane's velocity between 400 mph and 500 mph: ";
+    cin >> velocity;
+    myplane.setVelocity(velocity);
+
+    // Setting the timestep
+    double timestep;
+    cout << "Enter the timestep between 10 and 100 seconds: ";
+    cin >> timestep;
+
+    // Setting the number of iterations
+    double maxiterations;
+    cout << "Enter the maximum number of iterations between 1000 and 2000: ";
+    cin >> maxiterations;
+
+    for (double i = 0; i < maxiterations; ++i){
         myplane.operate(timestep);
-        cout << timestep * i << " " << myplane.getPosition() << endl;
+        cout << "Time:" << " " << timestep * i << " " << "Positon: " << myplane.getPosition() << " " <<"miles" << endl;
 
     }
     return 0;
