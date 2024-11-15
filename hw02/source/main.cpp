2c509af3ffc15060091aa48b813a889c99888224
wlertkitPSU <wkl5183@psu.edu>
2024-11-15 16:14:15 -0500

    finished q1

--- hw02/source/main.cpp
+++ hw02/source/main.cpp
@@ -1,3 +1,28 @@
+#include "q1.h"
+#include "sensorFactory.h"
+#include "aerospaceControlSystem.h"
+
 int main() {
+    // QUESTION 1
+    std::cout << "#################### QUESTION 1 ####################" << std::endl;
+
+    // Creating sensors
+    Sensor* tempSensorPtr = SensorFactory::createSensor("Temperature");
+    Sensor* altSensorPtr = SensorFactory::createSensor("Altitude");
+    Sensor* velSensorPtr = SensorFactory::createSensor("Airspeed");
+
+    // Control system
+    AerospaceControlSystem ctrlSys;
+    ctrlSys.addSensor(tempSensorPtr);
+    ctrlSys.addSensor(altSensorPtr);
+    ctrlSys.addSensor(velSensorPtr);
+    ctrlSys.monitorAndAdjust();
+
+    // Removing memory address after finishing to prevent memory leak
+    delete tempSensorPtr;
+    delete altSensorPtr;
+    delete velSensorPtr;
+
+    std::cout << "##################### END OF QUESTION 1 #####################" << std::endl;
     return 0;
 }
