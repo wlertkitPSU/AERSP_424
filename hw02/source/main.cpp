4940f56ba00ca98f014ef9e849808dbfb427fe13
wlertkitPSU <wkl5183@psu.edu>
2024-11-18 10:57:18 -0500

    added q4

--- hw02/source/main.cpp
+++ hw02/source/main.cpp
@@ -1,6 +1,7 @@
 #include "q1.h"
 #include "sensorFactory.h"
 #include "aerospaceControlSystem.h"
+#include "matplot/matplot.h"
 
 int main() {
     // QUESTION 1
@@ -24,5 +25,26 @@ int main() {
     delete velSensorPtr;
 
     std::cout << "##################### END OF QUESTION 1 #####################" << std::endl;
+
+    // QUESTION 4
+    std::cout << "#################### QUESTION 4 ####################" << std::endl;
+    
+    // Generating meshgrid
+    std::vector<double> x = matplot::iota(-5, 0.1, 0.5);
+    std::vector<double> y = matplot::iota(-5, 0.1, 0.5);
+    auto [X, Y] = matplot::meshgrid(x, y);
+
+    // Computing z = sin(sqrt(x^2 + y^2))
+    auto z = [](double x, double y){
+        return std::sin(std::sqrt(x*x + y*y));
+    };
+    auto Z = matplot::transform(X, Y, z);
+
+    // Plotting
+    auto fig = matplot::figure();
+    auto ax = fig->add_axes();
+    ax->surf(X, Y, Z);
+
+    std::cout << "##################### END OF QUESTION 4 #####################" << std::endl;
     return 0;
 }
