005d7d39643955fc05aa8f8338dab5673b7758d0
wlertkitPSU <wkl5183@psu.edu>
2024-12-09 03:37:06 -0500

    added main.cpp

---
+++ final_project/source/main.cpp
@@ -0,0 +1,12 @@
+#include <SDL.h>
+#include <iostream>
+
+int main() {
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        std::cerr << "Failed to initialize SDL: " << SDL_GetError() << std::endl;
+        return -1;
+    }
+    std::cout << "SDL initialized successfully!" << std::endl;
+    SDL_Quit();
+    return 0;
+}
