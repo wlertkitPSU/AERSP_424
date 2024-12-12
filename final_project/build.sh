0e5520b00e8b685db09067bd61ed07ba3ceb9592
wlertkitPSU <wkl5183@psu.edu>
2024-12-12 18:43:50 -0500

    got rid of external lib stuff

--- final_project/build.sh
+++ final_project/build.sh
@@ -3,24 +3,6 @@ current_directory="$(cd "$(dirname "$0")" && pwd)"
 project_root_dir=$current_directory
 cd ${project_root_dir}
 
-# Installing External Libraries
-echo -e "\nVerifying external libraries..."
-mkdir -p ${project_root_dir}/third_party
-cd ${project_root_dir}/third_party
-
-if [ ! -d "SDL" ]; then
-  echo "SDL not found, installing..."
-  git clone https://github.com/libsdl-org/SDL
-  cd ${project_root_dir}/third_party/SDL
-  rm -rf build install
-  mkdir -p build install
-  cd build
-  cmake -G Ninja -DCMAKE_INSTALL_PREFIX=${project_root_dir}/third_party/SDL/install -DCMAKE_BUILD_TYPE=Release ..
-  cmake --build . -j 4
-  cmake --install .
-fi
-echo "SDL installed!"
-
 # Compiling
 echo -e "\nCompiling..."
 cd ${project_root_dir}
