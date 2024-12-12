3997d339a1a67c301388cffdbed7659f28a6fd23
Emily Hartman <hartmane19@gmail.com>
2024-12-12 18:45:03 -0500

    All capital file names and fixed square

--- final_project/include/square.h
+++ final_project/include/square.h
@@ -1,21 +1,13 @@
 #pragma once
 #include "Pieces.h"
 
-class Square {
+class Square
+{
 public:
-    Square(int x = 0, int y = 0);
-    void setPiece(Pieces* pieces);   // Sets the piece on the square
-    Pieces* getPiece() const;       // Gets the piece on the square
-
-    // Additional methods
-    bool isOccupied() const;       // Checks if the square is occupied by a piece
-    int getX() const;              // Gets the x-coordinate of the square
-    int getY() const;              // Gets the y-coordinate of the square
-    int x, y;                      // Coordinates of the square
-    int occupied_value;            // 0 if unoccupied, 3 if king,2 if Queen, 1 if Rook, -1 if Knight, -2 if Bishop, -3 if Pawn
-    int occupied_color;            // 0 if unoccupied, 1 if white, -1 if black.
-    int cell_color; 
-
-private:
-    Pieces* pieces;                
+    Square();
+    int x, y;
+    int occupied_value; // 0 if unoccupied, 3 if king,2 if Queen, 1 if Rook, -1 if Knight, -2 if Bishop, -3 if Pawn
+    int occupied_color; // 0 if unoccupied, 1 if white, -1 if black.
+    int cell_color;     // 0 if black, 1 if white.
+    sf::RectangleShape square;
 };
--- final_project/source/square.cpp
+++ final_project/source/square.cpp
@@ -1,4 +1,4 @@
-#include "square.h"
+#include "Square.h"
 
 Square ::Square()
 {
